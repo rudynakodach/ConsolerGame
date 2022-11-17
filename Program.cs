17017038d36cec29ce5285fd4fed04f7e1016ed8
rudynakodach <whoppe7292@gmail.com>
2022-11-17 21:18:41 +0100

    initial commit

--- Program.cs
+++ Program.cs
@@ -3,229 +3,225 @@ using System.Threading;
 
 namespace Consoler
 {
-    class Program
-    {
+	class Program
+	{
 
-        public static string userName = Environment.UserName;
-        public static List<string> StartupQuotes = new List<string> { "The Internet? Is that thing still around?", "Chuck Norris counted to infinity... twice.", "C is quirky, flawed, and an enormous success", "Chuck Norris doesn’t go hunting. Chuck Norris goes killing.", "Hey! It compiles! Ship it!", "God is real, unless declared integer.", "It works on my machine.", "Keyboard not found...\nPress any key to continue...", "There is no place like 127.0.0.1", "There’s no test like production.", "Who is General Failure? And why is he reading my disk?", "I think we agree, the past is over.", "Linux is only free if your time has no value.", "On the Internet, nobody knows you’re a dog.", "One man’s constant is another man’s variable.", "PHP – Yeah, you know me." };
+		public static string userName = Environment.UserName;
+		public static List<string> StartupQuotes = new() { "The Internet? Is that thing still around?", "Chuck Norris counted to infinity... twice.", "C is quirky, flawed, and an enormous success", "Chuck Norris doesn’t go hunting. Chuck Norris goes killing.", "Hey! It compiles! Ship it!", "God is real, unless declared integer.", "It works on my machine.", "Keyboard not found...\nPress any key to continue...", "There is no place like 127.0.0.1", "There’s no test like production.", "Who is General Failure? And why is he reading my disk?", "I think we agree, the past is over.", "Linux is only free if your time has no value.", "On the Internet, nobody knows you’re a dog.", "One man’s constant is another man’s variable.", "PHP – Yeah, you know me." };
 
-        private static async Task StartupAnimation()
-        {
-            for (int i = 0; i < r.Next(10, 20); i++)
-            {
-                string currentQuote = StartupQuotes[r.Next(0, StartupQuotes.Count - 1)];
-                for (int x = 0; x < currentQuote.Length; x++)
-                {
-                    Console.Write(currentQuote[x]);
-                    Thread.Sleep(1);
-                }
-                Console.WriteLine("\n");
-            }
-            Console.SetCursorPosition(0, 0);
-            Console.ForegroundColor = ConsoleColor.DarkGray;
-            Console.BackgroundColor = ConsoleColor.DarkGray;
-            Thread.Sleep(750);
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.BackgroundColor = ConsoleColor.White;
-            Thread.Sleep(750);
-            Console.WriteLine("\n");
-            Console.ForegroundColor = ConsoleColor.DarkGray;
-            Console.BackgroundColor = ConsoleColor.DarkGray;
-            Thread.Sleep(750);
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.BackgroundColor = ConsoleColor.Black;
-            Thread.Sleep(750);
-            Console.Clear();
-        }
-
-
-
-        public static Random r = new();
-
-        public static void drawTextProgressBar(int progress, int total, ConsoleColor color, string message)
-        {
-            //draw empty progress bar
-            Console.CursorLeft = 0;
-            Console.Write("["); //start
-            Console.CursorLeft = 32;
-            Console.Write("]"); //end
-            Console.CursorLeft = 1;
-            float onechunk = 30.0f / total;
-
-            //draw filled part
-            int position = 1;
-            for (int i = 0; i < onechunk * progress; i++)
-            {
-                Console.BackgroundColor = color;
-                Console.CursorLeft = position++;
-                Console.Write(" ");
-            }
-
-            //draw unfilled part
-            for (int i = position; i <= 30; i++)
-            {
-                Console.BackgroundColor = ConsoleColor.Black;
-                Console.CursorLeft = position++;
-                Console.ForegroundColor = ConsoleColor.DarkGray;
-                Console.Write("#");
-                Console.ForegroundColor = ConsoleColor.White;
-            }
-
-            //draw the rest
-            Console.CursorLeft = 35;
-            Console.BackgroundColor = ConsoleColor.Black;
-            Console.Write(message);
-        }
-
-        public static async Task Main()
-        {
-            Console.WindowHeight = 50;
-            Console.WindowWidth = 75;
-
-            await StartupAnimation();
+		private static async Task StartupAnimation()
+		{
+			for (int i = 0; i < r.Next(10, 20); i++)
+			{
+				string currentQuote = StartupQuotes[r.Next(0, StartupQuotes.Count - 1)];
+				for (int x = 0; x < currentQuote.Length; x++)
+				{
+					Console.Write(currentQuote[x]);
+					Thread.Sleep(1);
+				}
+				Console.WriteLine("\n");
+			}
+			Console.SetCursorPosition(0, 0);
+			Console.ForegroundColor = ConsoleColor.DarkGray;
+			Console.BackgroundColor = ConsoleColor.DarkGray;
+			Thread.Sleep(750);
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.BackgroundColor = ConsoleColor.White;
+			Thread.Sleep(750);
+			Console.WriteLine("\n");
+			Console.ForegroundColor = ConsoleColor.DarkGray;
+			Console.BackgroundColor = ConsoleColor.DarkGray;
+			Thread.Sleep(750);
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.BackgroundColor = ConsoleColor.Black;
+			Thread.Sleep(750);
+			Console.Clear();
+		}
 
 
-            await Player.ExperienceBar();
-            Console.WriteLine("\nBOOT SUCCESSFUL.\nWelcome, {0}\n\n\n", userName);
 
-            await CommandInput();
-            //await Task.Run(Player.PlaytimeExperience);
-        }
+		public static Random r = new();
 
-        public static async Task CommandInput()
-        {
-            Console.Write("\n@root/user/home > ");
-            string? command = Console.ReadLine().ToLower();
+		public static void drawTextProgressBar(int progress, int total, ConsoleColor color, string message)
+		{
+			//draw empty progress bar
+			Console.CursorLeft = 0;
+			Console.Write("["); //start
+			Console.CursorLeft = 32;
+			Console.Write("]"); //end
+			Console.CursorLeft = 1;
+			float onechunk = 30.0f / total;
 
-            switch (command)
-            {
-                case "work":
-                    int workTime = r.Next(100, 250);
-                    Console.CursorVisible = false;
-                    for (int i = 0; i < workTime; i++)
-                    {
-                        drawTextProgressBar(i, workTime, ConsoleColor.Yellow, "Working...");
-                        await Task.Delay(50);
-                    }
-                    Console.CursorVisible = true;
-                    int moneyGained = r.Next(0, 100 * (Player.level) / 2);
-                    Player.money += moneyGained;
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine($"\n+{moneyGained}$");
-                    Console.ForegroundColor = ConsoleColor.White;
-                    break;
+			//draw filled part
+			int position = 1;
+			for (int i = 0; i < onechunk * progress; i++)
+			{
+				Console.BackgroundColor = color;
+				Console.CursorLeft = position++;
+				Console.Write(" ");
+			}
 
-                case "study":
-                    int studyTime = r.Next(250, 750);
-                    int experienceGained = r.Next(0, 50 * Player.level / 4);
-                    for (int i = 0; i < studyTime; i++)
-                    {
-                        drawTextProgressBar(i, studyTime, ConsoleColor.Cyan, "Studying...");
-                        await Task.Delay(150);
-                    }
-                    Player.GrantExperience(experienceGained);
-                    break;
+			//draw unfilled part
+			for (int i = position; i <= 30; i++)
+			{
+				Console.BackgroundColor = ConsoleColor.Black;
+				Console.CursorLeft = position++;
+				Console.ForegroundColor = ConsoleColor.DarkGray;
+				Console.Write("#");
+				Console.ForegroundColor = ConsoleColor.White;
+			}
 
-                case "stat":
-                    Console.WriteLine($"\nLevel: {Player.level}\nExperience: {Player.exp}/{Player.expToLevelUp}\nMoney: {Player.money}");
-                    break;
+			//draw the rest
+			Console.CursorLeft = 35;
+			Console.BackgroundColor = ConsoleColor.Black;
+			Console.Write(message);
+		}
 
-                case "sus": //sussy :eyes:
-                    Console.Beep(200, 300);
+		public static async Task Main()
+		{
+			Console.WindowHeight = 50;
+			Console.WindowWidth = 75;
 
-                    Thread.Sleep(350);
-
-                    Console.Beep(250, 300);
-                    Console.Beep(300, 300);
-                    Console.Beep(350, 300);
-                    Console.Beep(400, 300);
-                    Console.Beep(350, 300);
-                    Console.Beep(300, 300);
-                    Console.Beep(250, 300);
-
-                    Thread.Sleep(550);
-
-                    Console.Beep(250, 200);
-                    Console.Beep(300, 200);
-                    Console.Beep(250, 200);
-
-                    break;
-
-                case string s when s.StartsWith("game") && s.Split(' ', StringSplitOptions.RemoveEmptyEntries)[1].Trim() is "num":
-                    int correctNumber = r.Next(0, 10);
-                    Console.WriteLine("Guess a number between 0 - 10");
-                    int puzzleInput = int.Parse(Console.ReadLine());
-                    if (puzzleInput == correctNumber)
-                    {
-                        int expGranted = r.Next(0, 10 * Player.level / 4);
-                        int moneyGranted = r.Next(25, 100 * Player.level);
-
-                        Console.WriteLine("Your gess was correct!");
-                        Player.GrantExperience(expGranted);
-                        Player.money += moneyGranted;
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.WriteLine($"+{moneyGranted}$");
-                        Console.ForegroundColor = ConsoleColor.Cyan;
-                        Console.WriteLine($"+{expGranted} EXP");
-                        Console.ForegroundColor = ConsoleColor.White;
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Your guess was incorrect! The answer was {correctNumber}...");
-                    }
-                    break;
-
-                case "bg":
-                    Console.WriteLine("e");
-                    break;
-
-                default:
-                    Console.WriteLine("Unknown command.");
-                    break;
-            }
-            await CommandInput();
-        }
-    }
+			await StartupAnimation();
 
 
-    class Player
-    {
-        public static int money = 0;
-        public static int level = 1;
-        public static int expToLevelUp = 100;
-        public static int exp = 0;
+			await Player.ExperienceBar();
+			Console.WriteLine("\nBOOT SUCCESSFUL.\nWelcome, {0}\n\n\n", userName);
 
-        public static async Task PlaytimeExperience()
-        {
-            while (true)
-            {
-                await Task.Delay(5000);
-                GrantExperience(Program.r.Next(0, 100 * level / 2));
-            }
-        }
+			await CommandInput();
+			//await Task.Run(Player.PlaytimeExperience);
+		}
 
-        public static async Task ExperienceBar()
-        {
-            int x = Console.GetCursorPosition().Left;
-            int y = Console.GetCursorPosition().Top;
-            Console.SetCursorPosition(10, 4);
-            Program.drawTextProgressBar(exp, expToLevelUp, ConsoleColor.Cyan, "");
-            Console.SetCursorPosition(35, 5);
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine($"${money}");
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.SetCursorPosition(x, y);
-        }
+		public static async Task CommandInput()
+		{
+			Console.Write("\n@root/user/home > ");
+			string? command = Console.ReadLine().ToLower();
 
-        public static async Task GrantExperience(int expGained)
-        {
-            exp += expGained;
-            if (exp >= expToLevelUp)
-            {
-                exp -= expToLevelUp;
-                level++;
-            }
-            await ExperienceBar();
-        }
-    }
+			switch (command)
+			{
+				case "work":
+					int workTime = r.Next(100, 250);
+					Console.CursorVisible = false;
+					for (int i = 0; i < workTime; i++)
+					{
+						drawTextProgressBar(i, workTime, ConsoleColor.Yellow, "Working...");
+						await Task.Delay(50);
+					}
+					Console.CursorVisible = true;
+					int moneyGained = r.Next(0, 100 * (Player.level) / 2);
+					Player.money += moneyGained;
+					Console.ForegroundColor = ConsoleColor.Green;
+					Console.WriteLine($"\n+{moneyGained}$");
+					Console.ForegroundColor = ConsoleColor.White;
+					break;
+
+				case "study":
+					int studyTime = r.Next(250, 750);
+					int experienceGained = r.Next(0, 50 * Player.level / 4);
+					for (int i = 0; i < studyTime; i++)
+					{
+						drawTextProgressBar(i, studyTime, ConsoleColor.Cyan, "Studying...");
+						await Task.Delay(150);
+					}
+					Player.GrantExperience(experienceGained);
+					break;
+
+				case "stat":
+					Console.WriteLine($"\nLevel: {Player.level}\nExperience: {Player.exp}/{Player.expToLevelUp}\nMoney: {Player.money}");
+					break;
+
+				case "sus": //sussy :eyes:
+					Console.Beep(200, 300);
+
+					Thread.Sleep(350);
+
+					Console.Beep(250, 300);
+					Console.Beep(300, 300);
+					Console.Beep(350, 300);
+					Console.Beep(400, 300);
+					Console.Beep(350, 300);
+					Console.Beep(300, 300);
+					Console.Beep(250, 300);
+
+					Thread.Sleep(550);
+
+					Console.Beep(250, 200);
+					Console.Beep(300, 200);
+					Console.Beep(250, 200);
+
+					break;
+
+				case string s when s.StartsWith("game") && s.Split(' ', StringSplitOptions.RemoveEmptyEntries)[1].Trim() is "num":
+					int correctNumber = r.Next(0, 10);
+					Console.WriteLine("Guess a number between 0 - 10");
+					int puzzleInput = int.Parse(Console.ReadLine());
+					if (puzzleInput == correctNumber)
+					{
+						int expGranted = r.Next(0, 10 * Player.level / 4);
+						int moneyGranted = r.Next(25, 100 * Player.level);
+
+						Console.WriteLine("Your gess was correct!");
+						Player.GrantExperience(expGranted);
+						Player.money += moneyGranted;
+						Console.ForegroundColor = ConsoleColor.Green;
+						Console.WriteLine($"+{moneyGranted}$");
+						Console.ForegroundColor = ConsoleColor.Cyan;
+						Console.WriteLine($"+{expGranted} EXP");
+						Console.ForegroundColor = ConsoleColor.White;
+					}
+					else
+					{
+						Console.WriteLine($"Your guess was incorrect! The answer was {correctNumber}...");
+					}
+					break;
+
+				default:   
+					Console.WriteLine("Unknown command.");
+					break;
+			}
+			await CommandInput();
+		}
+	}
+
+
+	class Player
+	{
+		public static int money = 0;
+		public static int level = 1;
+		public static int expToLevelUp = 100;
+		public static int exp = 0;
+
+		public static async Task PlaytimeExperience()
+		{
+			while (true)
+			{
+				await Task.Delay(5000);
+				GrantExperience(Program.r.Next(0, 100 * level / 2));
+			}
+		}
+
+		public static async Task ExperienceBar()
+		{
+			int x = Console.GetCursorPosition().Left;
+			int y = Console.GetCursorPosition().Top;
+			Console.SetCursorPosition(10, 4);
+			Program.drawTextProgressBar(exp, expToLevelUp, ConsoleColor.Cyan, "");
+			Console.SetCursorPosition(35, 5);
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine($"${money}");
+			Console.ForegroundColor = ConsoleColor.White;
+			Console.SetCursorPosition(x, y);
+		}
+
+		public static async Task GrantExperience(int expGained)
+		{
+			exp += expGained;
+			if (exp >= expToLevelUp)
+			{
+				exp -= expToLevelUp;
+				level++;
+			}
+			await ExperienceBar();
+		}
+	}
 }
